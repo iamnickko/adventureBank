626e54befdf23d59e8363ded76bea6434f57214e
Nick Ko <nicholasedwardko@gmail.com>
2024-06-24 21:21:22 +0100

    feat: added form state and useSearchParams

---
+++ adventurebank/src/components/AuthForm.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const AuthForm = ({ isLoggingIn }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="mt-8 max-w-md mx-auto">
+      <form onSubmit={onSubmitHandler} className="grid grid-cols-1 gap-6">
+        {!isLoggingIn && (
+          <label htmlFor="name" className="block">
+            <span className="text-gray-700">Username</span>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="e.g. hikingChamp1337"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+          </label>
+        )}
+        <label htmlFor="email" className="block">
+          <span className="text-gray-700">Email</span>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email@domain.com"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label htmlFor="password" className="block">
+          <span>Password</span>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <button
+          className="mx-auto mb-5 w-1/3 border-2 border-lime-400 bg-lime-200/50 rounded-full"
+          type="submit"
+        >
+          {isLoggingIn ? "Login" : "Register"}
+        </button>
+      </form>
+      {isLoggingIn ? (
+        <Link to="/auth?mode=register" className="text-sm underline">
+          New? Register an account
+        </Link>
+      ) : (
+        <Link to="/auth?mode=login" className="text-sm underline">
+          Already registered? Login
+        </Link>
+      )}
+    </div>
+  );
+};
+export default AuthForm;
--- adventurebank/src/pages/Auth.jsx
+++ adventurebank/src/pages/Auth.jsx
@@ -1,42 +1,16 @@
+import { useSearchParams } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
 const Auth = () => {
+  const [searchParams] = useSearchParams();
+  const isLoggingIn = searchParams.get("mode") === "login";
+
   return (
     <>
-      <h1 className="text-3xl text-center">Auth page with form</h1>
-      <div className="mt-8 max-w-md mx-auto">
-        <form action="" className="grid grid-cols-1 gap-6">
-          <label htmlFor="name" className="block">
-            <span className="text-gray-700">Username</span>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="e.g. hikingChamp1337"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </label>
-          <label htmlFor="email" className="block">
-            <span className="text-gray-700">Username</span>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="email@domain.com"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </label>
-          <label htmlFor="password" className="block">
-            <span>Password</span>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="password"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <h1 className="text-3xl text-center">
+        {isLoggingIn ? "Login" : "Register"}
+      </h1>
+      <AuthForm isLoggingIn={isLoggingIn} />
     </>
   );
 };
