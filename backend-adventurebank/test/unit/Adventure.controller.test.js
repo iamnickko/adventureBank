fb670f86bfa848cb97bdafeacc2f7dd94ec7af3d
Nick Ko <nicholasedwardko@gmail.com>
2024-06-30 21:38:25 +0100

    feat: test - createAdventure responds with 201

--- backend-adventurebank/test/unit/Adventure.controller.test.js
+++ backend-adventurebank/test/unit/Adventure.controller.test.js
@@ -2,6 +2,13 @@ import { expect } from "chai";
 import sinon from "sinon";
 
 import AdventureController from "../../src/controllers/Adventure.controller.js";
+import testData from "../data/testData.js";
+
+// const { testAdventures } = testData;
+const testAdventures = [
+  { _id: 1, name: "abc", description: "xyz" },
+  { _id: 2, name: "def", description: "123" },
+];
 
 describe("AdventureController tests", () => {
   let adventureController;
@@ -11,9 +18,9 @@ describe("AdventureController tests", () => {
 
   beforeEach(() => {
     adventureService = {
-      createAdventure: sinon.stub().returnsThis(),
-      getAllAdventures: sinon.stub().returnsThis(),
-      deleteAdventure: sinon.stub().returnsThis(),
+      createAdventure: sinon.stub(),
+      getAllAdventures: sinon.stub(),
+      deleteAdventure: sinon.stub(),
     };
     adventureController = new AdventureController(adventureService);
     req = {
@@ -28,8 +35,10 @@ describe("AdventureController tests", () => {
   });
 
   describe("createAdventure tests", () => {
-    it("should respond with a 200 status code", async () => {
-      adventureService.createAdventure.resolves();
+    it("should respond with a 201 status code", async () => {
+      adventureService.createAdventure.resolves(testAdventures[0]);
+      await adventureController.createAdventure(req, res);
+      expect(res.status.calledWith(201)).to.be.true;
     });
   });
 });
