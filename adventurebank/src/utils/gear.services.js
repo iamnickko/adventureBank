bdfbe5ef1b969fa7d2661fb78804bde6f01b7230
Nick Ko <nicholasedwardko@gmail.com>
2024-07-01 21:38:24 +0100

    fix: await missing from axios call

--- adventurebank/src/utils/gear.services.js
+++ adventurebank/src/utils/gear.services.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { authHeader } from "./auth.services";
 
 export const createGear = async (formInput) => {
-  const response = axios.post(
+  const response = await axios.post(
     `${import.meta.env.VITE_APP_API}/gear`,
     { ...formInput },
     { headers: authHeader() }
@@ -18,8 +18,11 @@ export const getAllGear = async () => {
 };
 
 export const deleteGear = async (id) => {
-  const response = axios.delete(`${import.meta.env.VITE_APP_API}/gear/${id}`, {
-    headers: authHeader(),
-  });
+  const response = await axios.delete(
+    `${import.meta.env.VITE_APP_API}/gear/${id}`,
+    {
+      headers: authHeader(),
+    }
+  );
   return response.data;
 };
