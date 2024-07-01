3ffa0a7314655565ddb7b7e8281373f6a647df92
Nick Ko <nicholasedwardko@gmail.com>
2024-07-01 21:58:56 +0100

    feat: added tests getAllAdventures and deleteAdventure adventure.services

--- adventurebank/tests/utils/adventure.services.test.js
+++ adventurebank/tests/utils/adventure.services.test.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { authHeader } from "../../src/utils/auth.services";
-import { createAdventure } from "../../src/utils/adventure.services";
+import {
+  createAdventure,
+  getAllAdventures,
+  deleteAdventure,
+} from "../../src/utils/adventure.services";
 
 vi.mock("axios");
 vi.mock("../../src/utils/auth.services");
@@ -28,26 +32,50 @@ describe("adventure.service tests", () => {
       vi.clearAllMocks();
     });
 
-    it("should return the created adventure object", async () => {
-      axios.post.mockResolvedValue({ data: mockAdventure1 });
-      authHeader.mockReturnValue({ "X-Access-Token": "rich tea" });
+    describe("createAdventure tests", () => {
+      it("should return the created adventure object", async () => {
+        axios.post.mockResolvedValue({ data: mockAdventure1 });
+        authHeader.mockReturnValue({ "X-Access-Token": "rich tea" });
 
-      const newAdventure = await createAdventure(formInput);
+        const newAdventure = await createAdventure(formInput);
 
-      expect(newAdventure).toEqual(mockAdventure1);
+        expect(newAdventure).toEqual(mockAdventure1);
+      });
+
+      it("should throw an error if it fails", async () => {
+        const errorMessage = "an error";
+        axios.post.mockRejectedValue(new Error(errorMessage));
+        authHeader.mockReturnValue({ "X-Access-Token": "crumbly cookie" });
+
+        try {
+          await createAdventure(formInput);
+          throw new Error("making it throw an error");
+        } catch (error) {
+          expect(error.message).toContain(errorMessage);
+        }
+      });
     });
 
-    it("should throw an error if it fails", async () => {
-      const errorMessage = "an error";
-      axios.post.mockRejectedValue(new Error(errorMessage));
-      authHeader.mockReturnValue({ "X-Access-Token": "crumbly cookie" });
+    describe("getAllAdventures tests", () => {
+      it("should return an array of adventure objects", async () => {
+        axios.get.mockResolvedValue({ data: adventureArray });
+        authHeader.mockReturnValue({ "X-Access-Token": "oreo!" });
 
-      try {
-        await createAdventure(formInput);
-        throw new Error("making it throw an error");
-      } catch (error) {
-        expect(error.message).toContain(errorMessage);
-      }
+        const allAdventure = await getAllAdventures();
+        expect(allAdventure).toEqual(adventureArray);
+      });
+
+      describe("deleteAdventure tests", () => {
+        it("should return the adventure object that was deleted", async () => {
+          axios.delete.mockResolvedValue({ data: mockAdventure2 });
+          authHeader.mockReturnValue({
+            "X-Access-Token": "oreo!",
+          });
+
+          const deleteTheAdventure = await deleteAdventure(mockAdventure2._id);
+          expect(deleteTheAdventure).toEqual(mockAdventure2);
+        });
+      });
     });
   });
 });
