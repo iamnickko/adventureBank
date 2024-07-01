7ee95d13e9952631d82bec711b786f0578101392
Nick Ko <nicholasedwardko@gmail.com>
2024-07-01 21:12:24 +0100

    feat: added GearForm submit test

---
+++ adventurebank/tests/components/GearForm.test.jsx
@@ -0,0 +1,55 @@
+import { render, screen } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+import GearForm from "../../src/components/GearForm";
+import * as gearService from "../../src/utils/gear.services";
+
+vi.mock("../../src/utils/gear.services", () => ({
+  createGear: vi.fn(),
+}));
+const mockFetchAllGear = vi.fn();
+
+describe("GearForm tests", () => {
+  const renderWithRouter = () => {
+    const routes = [
+      {
+        path: "/gear",
+        element: <GearForm fetchAllGear={mockFetchAllGear} />,
+      },
+      { path: "/", element: <p>redirected</p> },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/gear"],
+    });
+    render(<RouterProvider router={router} />);
+  };
+
+  describe("Submit functionality", () => {
+    beforeEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it("should call createGear when the 'Add Gear' button is clicked", async () => {
+      renderWithRouter();
+      const nameInput = await screen.findByPlaceholderText(
+        "e.g. Osprey Rook 60L"
+      );
+      await userEvent.type(nameInput, "Sainsbury's Bag For Life");
+      const categorySelect = await screen.getByRole("combobox", {
+        name: "Choose a Category:",
+      });
+      await userEvent.selectOptions(categorySelect, "Backpack");
+      const descriptionInput = await screen.findByPlaceholderText(
+        "Add some details about your piece of gear..."
+      );
+      await userEvent.type(descriptionInput, "It's plastic");
+      const submitButton = await screen.getByRole("button", {
+        name: "Add Gear",
+      });
+      await userEvent.click(submitButton);
+      expect(gearService.createGear).toHaveBeenCalled();
+    });
+  });
+});
