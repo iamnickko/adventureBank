8089330ae30385fddb96dfd9abbe50aa7b2b1ec2
Nick Ko <nicholasedwardko@gmail.com>
2024-07-03 16:30:21 +0100

    fix: isAdmin state bug

--- adventurebank/src/App.jsx
+++ adventurebank/src/App.jsx
@@ -13,7 +13,7 @@ import Home from "./pages/Home";
 
 function App() {
   const [hasCookie, setHasCookie] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     if (checkForCookie()) {
