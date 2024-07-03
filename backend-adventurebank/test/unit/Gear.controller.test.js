9a2d44e60b89f558edd1f70e3d404b0f1397d80d
Nick Ko <nicholasedwardko@gmail.com>
2024-07-03 13:47:23 +0100

    feat: added allAllGear tests GearController

--- backend-adventurebank/test/unit/Gear.controller.test.js
+++ backend-adventurebank/test/unit/Gear.controller.test.js
@@ -24,6 +24,7 @@ describe("GearController tests", () => {
   let req;
   let res;
   let newGear;
+  let gearArray;
 
   beforeEach(() => {
     gearService = {
@@ -40,6 +41,7 @@ describe("GearController tests", () => {
       status: sinon.stub().returnsThis(),
       json: sinon.spy(),
     };
+    gearArray = [testGear[0], testGear[1]];
   });
 
   describe("createGear tests", () => {
@@ -65,7 +67,34 @@ describe("GearController tests", () => {
       const errorMessage = "we dropped the biscuits!";
       gearService.createGear.rejects(new Error(errorMessage));
       await gearController.createGear(req, res);
-      expect(res.json.calledWith({ message: errorMessage }));
+      expect(res.json.calledWith({ message: errorMessage })).to.be.true;
+    });
+  });
+
+  describe("getAllGear tests", () => {
+    it("should return a 200 status code", async () => {
+      gearService.getAllGear.resolves(gearArray);
+      await gearController.getAllGear(req, res);
+      expect(res.status.calledWith(200)).to.be.true;
+    });
+
+    it("should return an array of gear objects", async () => {
+      gearService.getAllGear.resolves(gearArray);
+      await gearController.getAllGear(req, res);
+      expect(res.json.calledWith(gearArray)).to.be.true;
+    });
+
+    it("should return a 500 status code if it fails", async () => {
+      gearService.getAllGear.rejects(new Error());
+      await gearController.getAllGear(req, res);
+      expect(res.status.calledWith(500)).to.be.true;
+    });
+
+    it("should return an error message if it fails", async () => {
+      const errorMessage = "perhaps one error message not about biscuits?";
+      gearService.getAllGear.rejects(new Error(errorMessage));
+      await gearController.getAllGear(req, res);
+      expect(res.json.calledWith({ message: errorMessage })).to.be.true;
     });
   });
 });
