203c861a36809861a2217822bee9952e091105ea
Nick Ko <nicholasedwardko@gmail.com>
2024-08-25 17:01:17 +0100

    UI update

--- adventurebank/src/pages/Adventures.jsx
+++ adventurebank/src/pages/Adventures.jsx
@@ -20,30 +20,28 @@ const Adventures = ({
       <h2 className="text-5xl pb-6 pt-4 text-center">Your Adventures</h2>
       <br />
       <br />
-      <div className="lg:grid lg:grid-cols-3 lg:gap-3">
+      <div className="lg:grid lg:grid-cols-2 lg:gap-3">
         <span className="lg:col-span-2">
           <AdventureList
             allAdventures={allAdventures}
             fetchAllAdventures={fetchAllAdventures}
           />
         </span>
-        <span>
-          {/* <AdventureForm handleSubmit={onSubmitCreateHandler} /> */}
-        </span>
-        <span>
-          <Link to="/adventures/new">
-            <button
-              className="mx-auto w-2/5 py-2 border-2 border-lime-400
+      </div>
+      <hr className="py-3 my-5 border-whiskey/50" />
+      <div className="text-center">
+        <Link to="/adventures/new">
+          <button
+            className="mx-auto w-2/5 py-2 border-2 border-lime-400
               bg-lime-200/50 rounded-full hover:bg-lime-400/50
               active:bg-lime-600/50 focus:outline-none focus:ring
               focus:ring-lime-300 disabled:bg-slate-50 disabled:text-slate-500
               disabled:border-slate-200 disabled:shadow-none
               disabled:cursor-not-allowed"
-            >
-              Create New Adventure
-            </button>
-          </Link>
-        </span>
+          >
+            Create New Adventure
+          </button>
+        </Link>
       </div>
     </>
   );
