fa5064f96e031909b14207cdaca19bb61c083f66
Nick Ko <nicholasedwardko@gmail.com>
2024-08-25 11:02:28 +0100

    refactor: error message. added props

--- adventurebank/src/components/GearTableRow.jsx
+++ adventurebank/src/components/GearTableRow.jsx
@@ -1,12 +1,19 @@
 import { deleteGear } from "../utils/gear.services";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-const GearTableRow = ({ gear, fetchAllGear }) => {
+const GearTableRow = ({
+  gear,
+  fetchAllGear,
+  setSelectedGear,
+  selectedGear,
+}) => {
   const navigate = useNavigate();
   const location = useLocation();
   const isGearPage = location.pathname === "/gear";
   const { id } = useParams();
-  const isAdventurePage = location.pathname === `/adventures/${id}`;
+  const isAdventurePage =
+    location.pathname === `/adventures/new` ||
+    location.pathname === `/adventures/${id}`;
 
   const handleDeleteClick = async (gearId) => {
     await deleteGear(gearId);
@@ -14,8 +21,19 @@ const GearTableRow = ({ gear, fetchAllGear }) => {
     navigate("/gear");
   };
 
+  const handleCheckboxChange = (e) => {
+    const { checked } = e.target;
+    if (checked) {
+      setSelectedGear((prevSelected) => [...prevSelected, gear]);
+    } else {
+      setSelectedGear((prevSelected) =>
+        prevSelected.filter((item) => item._id !== gear._id)
+      );
+    }
+  };
+
   return (
-    <tr className=" hover:bg-whiskey/5" key={gear._id}>
+    <tr className="hover:bg-whiskey/5" key={gear._id}>
       <td className="text-lg font-semibold px-4 py-2">{gear.name}</td>
       <td className="text-gray-500 uppercase text-sm font-semibold px-4 py-2">
         {gear.category}
@@ -36,9 +54,11 @@ const GearTableRow = ({ gear, fetchAllGear }) => {
         <td>
           <input
             type="checkbox"
-            id="gear._id"
-            name="gear._id"
+            id={gear.name}
+            name={gear.name}
             value={gear._id || ""}
+            checked={selectedGear.some((item) => item._id === gear._id)}
+            onChange={handleCheckboxChange}
           />
         </td>
       )}
