afb54480127b31ba0bb7fce213b2cde45a862410
Nick Ko <nicholasedwardko@gmail.com>
2024-08-25 11:06:03 +0100

    removed AdventureForm to separate page - replaced with create button with link

--- adventurebank/src/pages/Adventures.jsx
+++ adventurebank/src/pages/Adventures.jsx
@@ -2,6 +2,7 @@ import AdventureForm from "../components/AdventureForm";
 import AdventureList from "../components/AdventureList";
 import { createAdventure } from "../utils/adventure.services";
 import GearList from "../components/GearTable";
+import { Link } from "react-router-dom";
 
 const Adventures = ({
   allGear,
@@ -27,11 +28,22 @@ const Adventures = ({
           />
         </span>
         <span>
-          <AdventureForm handleSubmit={onSubmitCreateHandler} />
+          {/* <AdventureForm handleSubmit={onSubmitCreateHandler} /> */}
         </span>
-      </div>
-      <div>
-        <GearList allGear={allGear} fetchAllGear={fetchAllGear} />
+        <span>
+          <Link to="/adventures/new">
+            <button
+              className="mx-auto w-2/5 py-2 border-2 border-lime-400
+              bg-lime-200/50 rounded-full hover:bg-lime-400/50
+              active:bg-lime-600/50 focus:outline-none focus:ring
+              focus:ring-lime-300 disabled:bg-slate-50 disabled:text-slate-500
+              disabled:border-slate-200 disabled:shadow-none
+              disabled:cursor-not-allowed"
+            >
+              Create New Adventure
+            </button>
+          </Link>
+        </span>
       </div>
     </>
   );
